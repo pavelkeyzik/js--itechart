670d0fe6ec609330112f0aebbf9a35ba21c22800
Pavel Keizik <Pavel.Keizik@itechart-group.com>
2018-07-16 15:10:40 +0300

    ES5->ES6 binary-converter.js

--- 02_js-operations/src/binary-converter.js
+++ 02_js-operations/src/binary-converter.js
@@ -1,23 +1,26 @@
-function BinaryConverter(number) {
-  if(Array.isArray(number)) {
-    this._number = parseInt(number.reverse().join(''), 2);
-  } else if (Number.isInteger(number)) {
-    this._number = parseInt(number.toString(2), 2);
-  } else if (number.startsWith('#')) {
-    this._number = parseInt(number.substr(1), 16);
+class BinaryConverter {
+  
+  constructor(number) {
+    if(Array.isArray(number)) {
+      this._number = parseInt(number.reverse().join(''), 2);
+    } else if (Number.isInteger(number)) {
+      this._number = parseInt(number.toString(2), 2);
+    } else if (number.startsWith('#')) {
+      this._number = parseInt(number.substr(1), 16);
+    }
   }
-}
 
-BinaryConverter.prototype.toDec = function() {
-  return this._number;
-}
+  toDec() {
+    return this._number;
+  }
 
-BinaryConverter.prototype.toBinary = function() {
-  return parseInt(this._number.toString(2));
-}
+  toBinary() {
+    return parseInt(this._number.toString(2));
+  }
 
-BinaryConverter.prototype.toHex = function() {
-  return `#${this._number.toString(16).padStart(6, '0')}`;
+  toHex() {
+    return `#${this._number.toString(16).padStart(6, '0')}`;
+  }
 }
 
 export default BinaryConverter;
