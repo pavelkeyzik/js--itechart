c0d95903e132699926570d531f2b548fa2133a18
Pavel Keizik <Pavel.Keizik@itechart-group.com>
2018-07-16 15:08:39 +0300

    ES5->ES6 array-sorter.js

--- 02_js-operations/src/array-sorter.js
+++ 02_js-operations/src/array-sorter.js
@@ -1,80 +1,82 @@
-function ArraySorter(array) {
-  this._array = array;
-}
-
-ArraySorter.prototype.bubbleSort = function() {
-  let result = this._array.slice();
-
-  for(var i = 0; i < result.length; i++) {
-    for(var j = 0; j < result.length; j++) {
-      if(result[j] > result[j+1]) {
-        [result[j], result[j+1]] = [result[j+1], result[j]]
-      }    
-    }
+class ArraySorter {
+  constructor(array) {
+    this._array = array;
   }
 
-  return result;
-}
-
-ArraySorter.prototype.insertionSort = function() {
-  let array = this._array.slice();
-
-  for(var i = 1; i < array.length; i++) {
-    for(var j = i; j > 0; j--) {
-      if(array[j] < array[j-1]) {
-        [array[j], array[j-1]] = [array[j-1], array[j]];
+  bubbleSort() {
+    let result = this._array.slice();
+  
+    for(var i = 0; i < result.length; i++) {
+      for(var j = 0; j < result.length; j++) {
+        if(result[j] > result[j+1]) {
+          [result[j], result[j+1]] = [result[j+1], result[j]]
+        }    
       }
     }
+  
+    return result;
   }
 
-  return array;
-}
-
-ArraySorter.prototype.quickSort = function() {
-  let array = this._array.slice();
-  _quick(array, 0, array.length - 1);
-  return array;
-}
-
-function _quick(array, low, high) {
-  if(high <= low) return;
-
-  var i = low;
-  var j = high + 1;
-
-  do {
-    while(array[++i] < array[low]) {
-      if(i == high) break;
+  insertionSort() {
+    let array = this._array.slice();
+  
+    for(var i = 1; i < array.length; i++) {
+      for(var j = i; j > 0; j--) {
+        if(array[j] < array[j-1]) {
+          [array[j], array[j-1]] = [array[j-1], array[j]];
+        }
+      }
     }
+  
+    return array;
+  }
 
-    while(array[--j] > array[low]) {
-      if(j == low) break;
+  quickSort() {
+    let array = this._array.slice();
+    this._quick(array, 0, array.length - 1);
+    return array;
+  }
+
+  selectionSort() {
+    let array = this._array.slice();
+  
+    for(var i = 0; i < array.length; i++) {
+      for(var j = i + 1; j < array.length; j++) {
+        if(array[i] > array[j]) {
+          [array[i], array[j]] = [array[j], array[i]];
+        }
+      }
     }
+  
+    return array;
+  }
 
-    if(i < j) {
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-
-  } while(i < j);
-
-  [array[low], array[j]] = [array[j], array[low]];
-
-  _quick(array, low, j - 1);
-  _quick(array, j+1, high);
-}
-
-ArraySorter.prototype.selectionSort = function() {
-  let array = this._array.slice();
-
-  for(var i = 0; i < array.length; i++) {
-    for(var j = i + 1; j < array.length; j++) {
-      if(array[i] > array[j]) {
+  _quick(array, low, high) {
+    if(high <= low) return;
+  
+    var i = low;
+    var j = high + 1;
+  
+    do {
+      while(array[++i] < array[low]) {
+        if(i == high) break;
+      }
+  
+      while(array[--j] > array[low]) {
+        if(j == low) break;
+      }
+  
+      if(i < j) {
         [array[i], array[j]] = [array[j], array[i]];
       }
-    }
+  
+    } while(i < j);
+  
+    [array[low], array[j]] = [array[j], array[low]];
+  
+    this._quick(array, low, j - 1);
+    this._quick(array, j+1, high);
   }
-
-  return array;
 }
 
 export default ArraySorter;
