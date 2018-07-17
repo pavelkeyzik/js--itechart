9aaf4e45f9ee6c5d7ea68e88099bbd560e2e8d44
Pavel Keizik <Pavel.Keizik@itechart-group.com>
2018-07-17 16:31:20 +0300

    Added return array was called upon

--- 03_functional-advanced/linear-fold.js
+++ 03_functional-advanced/linear-fold.js
@@ -4,8 +4,7 @@ function linearFold(array, cb, initialValue) {
   const startIndex = initialValue ? 0 : 1;
 
   for(let i = startIndex; i < array.length; i++) {
-    result = cb(previousValue, array[i], i, 4);
-    cb(previousValue, result, i,2);
+    result = cb(previousValue, array[i], i, array.slice(0, i + 1));
     previousValue = result;
   }
 
@@ -14,7 +13,16 @@ function linearFold(array, cb, initialValue) {
 
 const arrayForTesting = [1, 2, 3, 4, 5];
 
-const result = linearFold(arrayForTesting, (previousValue, currentValue) => previousValue + currentValue, 0);
-console.log('RESULT:', result);
+const resultWithoutInitial = linearFold(arrayForTesting, (previousValue, currentValue) => {
+  return previousValue + currentValue;
+});
+
+const resultWithInitial = linearFold(arrayForTesting, (previousValue, currentValue, index, arr) => {
+  console.log(index, arr);
+  return previousValue + currentValue;
+}, 15);
+
+console.log('RESULT WITHOUT INITIAL:', resultWithoutInitial);
+console.log('RESULT WITH INITIAL:', resultWithInitial);
 
 module.exports = linearFold;
