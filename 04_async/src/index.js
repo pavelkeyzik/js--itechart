3b6c18cf657c2768de81cc422c08dcd46d9e12ea
Pavel Keyzik <pavel.keyzik@gmail.com>
2018-07-23 17:20:31 +0300

    Moved render items to function

--- 04_async/src/index.js
+++ 04_async/src/index.js
@@ -8,30 +8,35 @@ submitButton.addEventListener('click', (ev) => {
   if (!value) return;
   promiseBlock.innerHTML = 'is loading...';
 
-  getFromApi(value)
+  getFromApiPromise(value)
     .then((data) => {
-      let string = '';
-
-      data.DailyForecasts.forEach((item) => {
-        const temperature = item.Temperature;
-        string += `<div>
-          <hr>
-          <h3>${new Date(item.Date).toDateString()}</h3>
-          <div>
-            Min <b>${temperature.Minimum.Value}${temperature.Minimum.Unit}</b>
-            Max <b>${temperature.Maximum.Value}${temperature.Maximum.Unit}</b>
-          </div>
-        </div>`;
-      });
-      promiseBlock.innerHTML = string;
+      promiseBlock.innerHTML = renderItems(data.DailyForecasts);
     })
     .catch(() => promiseBlock.innerHTML = 'City not found...');
 });
 
-function getFromApi(city) {
+function getFromApiPromise(city) {
   return fetch(`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=tokKLS48aSf50X3PHoQQ13PQIh3Lo7mM&q=${city}`)
     .then(res => res.json())
     .then(items => items[0].Key)
     .then(cityKey => fetch(`http://dataservice.accuweather.com/forecasts/v1/daily/5day/${cityKey}?apikey=tokKLS48aSf50X3PHoQQ13PQIh3Lo7mM`))
     .then(d => d.json());
+}
+
+function renderItems(items) {
+  let string = '';
+
+  items.forEach((item) => {
+    const temperature = item.Temperature;
+    string += `<div>
+      <hr>
+      <h3>${new Date(item.Date).toDateString()}</h3>
+      <div>
+        Min <b>${temperature.Minimum.Value}${temperature.Minimum.Unit}</b>
+        Max <b>${temperature.Maximum.Value}${temperature.Maximum.Unit}</b>
+      </div>
+    </div>`;
+  });
+
+  return string;
 }
