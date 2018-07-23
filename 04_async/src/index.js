cf201212aaa9a0b8ae0b41699eb5e163fe2702ee
Pavel Keyzik <pavel.keyzik@gmail.com>
2018-07-23 17:12:41 +0300

    Removed MVC pattern

--- 04_async/src/index.js
+++ 04_async/src/index.js
@@ -1,9 +1,37 @@
-import ListController from './controller/ListController';
-import ListModel from './model/ListModel';
-import ListView from './view/ListView';
+const searchInput = document.getElementById('search-input'),
+  submitButton = document.getElementById('submit-button'),
+  promiseBlock = document.getElementById('promise');
 
-const listModel = new ListModel(['first', 'second', 'third']);
-const listView = new ListView(listModel, {
-  list: document.getElementById('promise'),
+submitButton.addEventListener('click', (ev) => {
+  ev.preventDefault();
+  const value = searchInput.value;
+  if (!value) return;
+  promiseBlock.innerHTML = 'is loading...';
+
+  getFromApi(value)
+    .then((data) => {
+      let string = '';
+
+      data.DailyForecasts.forEach((item) => {
+        const temperature = item.Temperature;
+        string += `<div>
+          <hr>
+          <h3>${new Date(item.Date).toDateString()}</h3>
+          <div>
+            Min <b>${temperature.Minimum.Value}${temperature.Minimum.Unit}</b>
+            Max <b>${temperature.Maximum.Value}${temperature.Maximum.Unit}</b>
+          </div>
+        </div>`;
+      });
+      promiseBlock.innerHTML = string;
+    })
+    .catch(() => promiseBlock.innerHTML = 'City not found...');
 });
-const listController = new ListController(listModel, listView);
+
+function getFromApi(city) {
+  return fetch(`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=tokKLS48aSf50X3PHoQQ13PQIh3Lo7mM&q=${city}`)
+    .then(res => res.json())
+    .then(items => items[0].Key)
+    .then(cityKey => fetch(`http://dataservice.accuweather.com/forecasts/v1/daily/5day/${cityKey}?apikey=tokKLS48aSf50X3PHoQQ13PQIh3Lo7mM`))
+    .then(d => d.json());
+}
