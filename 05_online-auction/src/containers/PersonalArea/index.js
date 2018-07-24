253a1ac95a124e11ece3d32fbdaa962c3f9ea5fe
Pavel Keyzik <pavel.keyzik@gmail.com>
2018-07-24 14:21:46 +0300

    Init 05_task with Router

---
+++ 05_online-auction/src/containers/PersonalArea/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class PersonalArea extends React.Component {
+  render() {
+    return (
+      <h1>Personal Area Page</h1>
+    );
+  }
+}
+
+export default PersonalArea;
