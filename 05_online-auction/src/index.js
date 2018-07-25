939d6a65d61706815501c6ec3973938dbff11e76
Pavel Keyzik <pavel.keyzik@gmail.com>
2018-07-25 10:19:52 +0300

    Added redux and personalArea information

--- 05_online-auction/src/index.js
+++ 05_online-auction/src/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '@/containers/App';
 import { BrowserRouter } from 'react-router-dom';
+import appReducers from '@/reducers';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+const store = createStore(appReducers);
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
---
+++ 05_online-auction/src/reducers/personalArea.js
@@ -0,0 +1,14 @@
+import * as typeAction from '@/actions/actions';
+
+const personalAreaReducer = (state = {isLoading: true}, action) => {
+  switch (action.type) {
+  case typeAction.PERSONAL_AREA_IS_LOADING:
+    return {...state, isLoading: true};
+  case typeAction.PERSONAL_AREA_IS_LOADED:
+    return {...state, userInformation: action.payload, isLoading: false};
+  default:
+    return state;
+  }
+};
+
+export default personalAreaReducer;
