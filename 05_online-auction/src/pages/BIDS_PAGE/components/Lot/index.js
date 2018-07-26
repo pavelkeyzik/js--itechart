48f9e9aff146fddb1fc6cf80470badf6e9501d09
Pavel Keyzik <pavel.keyzik@gmail.com>
2018-07-26 16:09:18 +0300

    Added styles and moment.js for working with dates

--- 05_online-auction/src/pages/BIDS_PAGE/components/Lot/index.js
+++ 05_online-auction/src/pages/BIDS_PAGE/components/Lot/index.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import './index.scss';
+import moment from 'moment';
 
 class Lot extends Component {
   render() {
@@ -8,15 +10,15 @@ class Lot extends Component {
       <div className="lot">
         <div className="lot__head">
           <div className="lot__time-to-end">
-            Time to end {data.time_to_end}$
+            {moment().to(moment(data.time_to_end))}
           </div>
           <div className="lot__image">
             <img width="150" src={data.image_url} alt={data.description}/>
           </div>
-          <div className="lot__current-bid">Current bid: {data.current_bid}</div>
+          <div className="lot__current-bid">Current bid: <b>{data.current_bid}$</b></div>
         </div>
         <div className="lot__bottom">
-          <div className="lot__information">#{data.id} {data.description}</div>
+          <div className="lot__information">{data.description}</div>
           <div className="lot__buttons">
             <button className="lot__button">+5%</button>
             <button className="lot__button">+10%</button>
