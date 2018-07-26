d537e35cf8a2dad4612009b1da8b73901f8f3f18
Pavel Keyzik <pavel.keyzik@gmail.com>
2018-07-26 17:27:50 +0300

    Added timer to end a lot

--- 05_online-auction/src/shared/data/lots.js
+++ 05_online-auction/src/shared/data/lots.js
@@ -3,14 +3,14 @@ export default [
     id: 1,
     image_url: 'http://www.robbiesports.cc/images/2017/CF98E06F3B7C0C4F871CA5A237E09AA8.jpg',
     description: 'First lot',
-    time_to_end: 1533805200000,
+    time_to_end: '2018-07-26T14:51:33.703Z',
     current_bid: 15,
   },
   {
     id: 2,
     image_url: 'http://aglschoolspirit.com/wp-content/uploads/2017/01/Wood-6010-Mens-Vintage-black-800x1000.jpg',
     description: 'Secondlot',
-    time_to_end: 1532817200000,
+    time_to_end: '2018-07-26T16:21:33.703Z',
     current_bid: 11,
   }
 ];
