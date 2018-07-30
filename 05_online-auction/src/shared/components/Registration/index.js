9af5539b6d6ebb5dd9b265735ee546b0f7078407
Pavel Keyzik <pavel.keyzik@gmail.com>
2018-07-30 11:25:19 +0300

    Added Authorization Component and moved form style to shared

--- 05_online-auction/src/shared/components/Registration/index.js
+++ 05_online-auction/src/shared/components/Registration/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import './index.scss';
+import '@/shared/styles/form.scss';
 
 class Registration extends Component {
   render() {
