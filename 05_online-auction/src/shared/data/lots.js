72e5c670fd81f5f32da320624fb925caf562a559
Pavel Keyzik <pavel.keyzik@gmail.com>
2018-07-31 12:10:55 +0300

    Changed Lot Component UI styles and shared/data/lots

--- 05_online-auction/src/shared/data/lots.js
+++ 05_online-auction/src/shared/data/lots.js
@@ -1,16 +1,42 @@
 export default [
   {
     id: 1,
-    image_url: 'http://www.robbiesports.cc/images/2017/CF98E06F3B7C0C4F871CA5A237E09AA8.jpg',
-    description: 'First lot',
+    image_url: 'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=f9f0fdc18a215ec725f8ca61dc6fcbdf&auto=format&fit=crop&w=450&q=80',
+    description: 'Headphones for my beautiful life sucks',
     time_to_end: '2018-07-26T14:51:33.703Z',
-    current_bid: 15,
+    current_bid: 7,
+    category: 'electronics',
   },
   {
     id: 2,
-    image_url: 'http://aglschoolspirit.com/wp-content/uploads/2017/01/Wood-6010-Mens-Vintage-black-800x1000.jpg',
-    description: 'Secondlot',
+    image_url: 'https://images.unsplash.com/photo-1515955656352-a1fa3ffcd111?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=93ac7738d6d00bbe2b003cbe833300cd&auto=format&fit=crop&w=450&q=80',
+    description: 'Blue Sneakers',
     time_to_end: '2018-07-26T16:21:33.703Z',
-    current_bid: 11,
+    current_bid: 95,
+    category: 'clothes',
+  },
+  {
+    id: 3,
+    image_url: 'https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=df4d2256d54f07d2d40ce7cda76c7ebf&auto=format&fit=crop&w=450&q=80',
+    description: 'Polaroid camera',
+    time_to_end: '2018-07-26T16:21:33.703Z',
+    current_bid: 125,
+    category: 'electronics',
+  },
+  {
+    id: 4,
+    image_url: 'https://images.unsplash.com/photo-1524738258074-f8125c6a7588?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d6f362ba884412a9f8f4f1a91dac162e&auto=format&fit=crop&w=450&q=80',
+    description: 'Watches',
+    time_to_end: '2018-07-26T16:21:33.703Z',
+    current_bid: 80,
+    category: 'Accessories',
+  },
+  {
+    id: 5,
+    image_url: 'https://images.unsplash.com/photo-1501951653466-8df816debe46?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=86cc25ffd0790b8e5446b5ea06902987&auto=format&fit=crop&w=450&q=80',
+    description: 'Watches',
+    time_to_end: '2018-07-26T16:21:33.703Z',
+    current_bid: 2,
+    category: 'Accessories',
   }
 ];
