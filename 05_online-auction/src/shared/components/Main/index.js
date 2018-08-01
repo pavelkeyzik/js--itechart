127765bf3623849c4808523d90d80a4aac0d20a7
Pavel Keyzik <pavel.keyzik@gmail.com>
2018-08-01 11:45:56 +0300

    Implementing LeftMenu Component in Main Component

--- 05_online-auction/src/shared/components/Main/index.js
+++ 05_online-auction/src/shared/components/Main/index.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
-import TopNavigation from '../TopNavigation';
+// import TopNavigation from '../TopNavigation';
+import LeftNavigation from '../LeftNavigation';
 import { Switch, Route } from 'react-router-dom';
 import { Redirect } from 'react-router';
 import RouteWithSubRoutes from '../RouteWithSubRoutes';
+import './index.scss';
 
 class Main extends Component {
   render() {
     return (
       <Route>
-        <div>
+        <div className="main">
         {!localStorage.getItem('authorizedUserToken') && <Redirect to="/" />}
         {localStorage.getItem('authorizedUserToken') &&
           <div>
-            <TopNavigation />
-            <Switch>
-              {this.props.routes.map((route, i) => <RouteWithSubRoutes key={i} {...route} />)}
-            </Switch>
+            <div className="main__container">
+              <div className="main__navigation">
+                <LeftNavigation />
+              </div>
+              <Switch>
+                {this.props.routes.map((route, i) => <RouteWithSubRoutes key={i} {...route} />)}
+              </Switch>
+            </div>
           </div>
         }
         </div>
