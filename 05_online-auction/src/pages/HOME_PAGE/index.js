d0c9c713d19491bea6ad7a5e4444e73d70a1f126
Pavel Keyzik <pavel.keyzik@gmail.com>
2018-08-01 14:50:02 +0300

    Added Home page content

--- 05_online-auction/src/pages/HOME_PAGE/index.js
+++ 05_online-auction/src/pages/HOME_PAGE/index.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import './index.scss';
 
 class HomePage extends Component {
   render() {
     return (
-      <div>
-        <h1>HOME PAGE</h1>
+      <div className="home">
+        <div className="qr-code">
+          <img src="/images/qr-codes/pavelkeyzik-github.svg" alt="QR-code pavelkeyzik.github.io"/>
+        </div>
+        <div className="home__description">Don't scan me! Please do not!</div>
       </div>
     );
   }
