a5ed1fedade56ca47e7e08f6a40c3ba53bc29fbd
Pavel Keyzik <pavel.keyzik@gmail.com>
2018-08-10 14:14:00 +0300

    Added capability to get images from the server

--- 06_online-auction-api/app/index.js
+++ 06_online-auction-api/app/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const routes = require('./routes');
 const bodyParser = require('body-parser');
 const app = express();
+const path = require('path');
 
 app.use(bodyParser.json());
 app.use(routes);
+app.use('/assets', express.static(path.resolve(__dirname, '../public')));
 
 module.exports = app;
