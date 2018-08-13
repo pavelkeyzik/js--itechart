ff814a280a7a53658516110a145b6425f01bbd2a
Pavel Keyzik <pavel.keyzik@gmail.com>
2018-08-13 12:58:15 +0300

    Changed url string parser to new

--- 06_online-auction-api/db.js
+++ 06_online-auction-api/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 const config = require('./app/config');
-const db = mongoose.createConnection(config.dataBaseURL);
+const db = mongoose.createConnection(config.dataBaseURL, { useNewUrlParser: true });
 
 module.exports = db;
