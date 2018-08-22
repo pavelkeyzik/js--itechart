c0aef27fe514c153f01b16a99f10a109ef9a68a6
Pavel Keyzik <pavel.keyzik@gmail.com>
2018-08-22 16:21:28 +0300

    Added spinner on authorization

---
+++ 05_online-auction/src/shared/components/AuthentificationLoader/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './index.scss';
+
+export default () => (
+  <div className="authentification-loader">
+    <div className="authentification-loader__spinner"></div>
+  </div>
+);
