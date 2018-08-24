e9a79e5f4ff3b49a69a2fb6116a6fc9b90396c3f
Pavel Keyzik <pavel.keyzik@gmail.com>
2018-08-24 16:03:58 +0300

    Fixed timer format for lot

--- 05_online-auction/src/pages/BIDS_PAGE/components/Lot/index.js
+++ 05_online-auction/src/pages/BIDS_PAGE/components/Lot/index.js
@@ -103,11 +103,25 @@ class Lot extends PureComponent {
         return;
       }
       this.setState({
-        timer: `${d.hours()}h ${d.minutes()}m ${d.seconds()}s`,
+        timer: this.timerFormat(d),
         canRiseUp: true,
       })
     }, 1000);
-  } 
+  }
+
+  timerFormat(date) {
+    if(date.years()) {
+      return `${date.years()}y ${date.days()}d ${date.hours()}h ${date.minutes()}m ${date.seconds()}s`;
+    } else if (date.days() > 0) {
+      return `${date.days()}d ${date.hours()}h ${date.minutes()}m ${date.seconds()}s`;
+    } else if (date.hours()) {
+      return `${date.hours()}h ${date.minutes()}m ${date.seconds()}s`;
+    } else if (date.minutes()) {
+      return `${date.minutes()}m ${date.seconds()}s`;
+    } else {
+      return `${date.seconds()}s`;
+    }
+  }
 }
 
 Lot.propTypes = {
